cc171864274c278321424c022c61331dd6b11dad
Niicck <niicck@users.noreply.github.com>
2019-11-05 16:06:28 -0600

    hot reload module is pointless

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,5 @@ module.exports = {
   externals: [nodeExternals()],
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    function(compiler) {
-      console.log("~~~~ file dependencies?", this.fileDependencies)
-      console.log("~~~~ contextDependencies?", this.contextDependencies)
-      // compiler.plugin("after-compiler", function() {
-        // this.fileDependencies.push("/path/to/my/file");
-        // this.contextDependencies.push("/path/to/my/folder");
-      // }
-    },
   ],
 };
