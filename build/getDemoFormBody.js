0686ca06b1c2277abee5f243067669bd7fdc1aab
Niicck <niicck@users.noreply.github.com>
2019-11-08 17:00:24 -0600

    add options for demo_form_body

--- build/getDemoFormBody.js
+++ build/getDemoFormBody.js
@@ -12,7 +12,7 @@ dotenv.config();
   If you want to demo your own form, there are a couple of extra steps.
   You need to use or generate an Access Token from Formstack's API.
   Plug in the FORM_ID of your form and your FORMSTACK_ACCESS_TOKEN into a .env file.
-  Run this script to write the body of your form to /src/preview/demo_form_body_[FORM_ID].html
+  Run this script to write the body of your form to /src/partials/html/demo_form_body_[FORM_ID].html
 */
 console.log(`Fetching form ${process.env.FORM_ID}`)
 axios.get(`https://www.formstack.com/api/v2/form/${process.env.FORM_ID}.json`, {
@@ -22,7 +22,7 @@ axios.get(`https://www.formstack.com/api/v2/form/${process.env.FORM_ID}.json`, {
 }).then((res) => {
   const $ = cheerio.load(res.data.html);
   const body = pretty($("body").html())
-  fs.writeFileSync(path.resolve(__dirname, `./demo_form_body_${process.env.FORM_ID}.html`), body)
+  fs.writeFileSync(path.resolve(__dirname, `../src/partials/html/demo_form_body_${process.env.FORM_ID}.html`), body)
   console.log(`New form body written to src/preview/demo_form_body_${process.env.FORM_ID}.html`)
 }).catch((err) => {
   console.log(err)
--- build/preview.js
+++ build/preview.js
@@ -24,11 +24,16 @@ const options = {
 }
 hbsutils.registerWatchedPartials(partialsDir, options)
 
+const data = {
+  isProduction: false,
+  demoFormBodyPartial: process.env.DEMO_FORM_BODY_PARTIAL || "demo_form_body_single_page",
+}
+
 // Start express server with Handlebars view engine
 app.set("view engine", "hbs");
 app.set('views', __dirname + '/../src/');
 app.get('/', (req,res) => {
-  res.render('preview');
+  res.render('preview', data);
 });
 const port = process.env.FORMSTACK_PREVIEW_PORT || 5000
 app.listen(port);
