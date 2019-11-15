05c61f13f94008a10a58ae21fda05229633027c4
Niicck <niicck@users.noreply.github.com>
2019-11-15 16:48:03 -0600

    add data-iframe-height for iFrameResizer

---
+++ src/partials/js/addDataIframeHeight.js
@@ -0,0 +1,8 @@
+$ = jQuery;
+
+/*
+  Allows iframeResizer to know exactly how large the iframe should be.
+  Parent iframeResizer must use "taggedElement" method
+  https://github.com/davidjbradshaw/iframe-resizer/blob/master/docs/parent_page/options.md#heightcalculationmethod
+*/
+$('.fsForm').attr('data-iframe-height', 'true');
