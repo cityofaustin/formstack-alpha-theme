0b57ef5b095913a46e652a077eaebd5feba98f76
Bob Main <bobmain49@gmail.com>
2020-02-04 15:49:04 -0600

    fixed margin issue and adding form bug

--- build/getDemoFormBody.js
+++ build/getDemoFormBody.js
@@ -15,17 +15,18 @@ dotenv.config();
   Run this script to write the body of your form to /src/partials/html/demo_form_body_[FORM_ID].html
 */
 console.log(`Fetching form ${process.env.FORM_ID}`)
+console.log()
 axios.get(`https://www.formstack.com/api/v2/form/${process.env.FORM_ID}.json`, {
   headers: {
     "Authorization": `Bearer ${process.env.FORMSTACK_ACCESS_TOKEN}`
   }
 }).then((res) => {
   const $ = cheerio.load(res.data.html);
-  // Strangely, the API doesn't set the "$" for currency fields, even though the real site will render it. 
+  // Strangely, the API doesn't set the "$" for currency fields, even though the real site will render it.
   $(".fsCurrencyPrefix").text('$');
   const body = pretty($("body").html())
-  fs.writeFileSync(path.resolve(__dirname, `../src/partials/html/demo_form_body_${process.env.FORM_ID}.html`), body)
-  console.log(`New form body written to src/preview/demo_form_body_${process.env.FORM_ID}.html`)
+  fs.writeFileSync(path.resolve(__dirname, `../src/partials/html/${process.env.DEMO_FORM_BODY_PARTIAL}.html`), body)
+  console.log(`New form body written to src/preview/${process.env.DEMO_FORM_BODY_PARTIAL}.html`)
 }).catch((err) => {
   console.log(err)
 })
